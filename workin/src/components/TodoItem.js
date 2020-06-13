fe6cb66c4f682490ffc7afb27e9a64638884b524
karatugo <karatugo@gmail.com>
2020-06-13 17:53:31 +0300

    Todo App - Phase 6: change state of todo items

--- workin/src/components/TodoItem.js
+++ workin/src/components/TodoItem.js
@@ -3,7 +3,12 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="card form-check">
-            <input type="checkbox" className="form-check-input" checked={props.item.completed} />
+            <input
+                type="checkbox"
+                className="form-check-input"
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
+            />
             <label className="form-check-label">{props.item.text}</label>
         </div>
     )
