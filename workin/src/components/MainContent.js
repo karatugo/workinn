d42ccc0b9141484ad1f7be1fce0ce45c94a9b381
karatugo <karatugo@gmail.com>
2020-06-13 17:23:30 +0300

    Todo app - phase 4

--- workin/src/components/MainContent.js
+++ workin/src/components/MainContent.js
@@ -2,17 +2,27 @@ import React from "react"
 import TodoItem from './TodoItem';
 import todosData from './todosData'
 
-function MainContent() {
+class MainContent extends React.Component {
 
-    const todoComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
 
-    return (
-        <main>
-            <div className="todo-list" style={{}}>
-                {todoComponents}
-            </div>
-        </main>
-    )
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+
+    render() {
+        const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+
+        return (
+            <main>
+                <div className="todo-list" style={{}}>
+                    {todoComponents}
+                </div>
+            </main >
+        )
+    }
 }
 
 export default MainContent
